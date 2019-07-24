8297e21949021b93840c520a8c3b5c3f31664d5b
kwame16 <51427903+kwame16@users.noreply.github.com>
2019-07-24 16:13:12 -0400

    added if/else statements

--- liri.js
+++ liri.js
@@ -1,3 +1,21 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+
+if (command === "concert-this"){
+    console.log("it went through here")
+}
+else if(command === "spotify-this-song"){
+    console.log("it went through here1")
+}
+else if(command === "movie-this"){
+    console.log("it went through here2")
+}
+else if(command === "do-what-it-says"){
+    console.log("it went through here3")
+}
+else{
+    console.log("default")
+}
